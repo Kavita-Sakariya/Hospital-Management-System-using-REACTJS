48a6de9d12272b1dbe116bf2253994f86997fe56
Kavita Sakariya <kavitasoni141103@gmail.com>
2024-03-23 23:43:55 +0530

    create homepage

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 
 const HomePage = () => {
   return (
-    <div className="homepage" style={{textAlign: 'center'}}>HomePage</div>
+    <>
+    <div className="homepage" style={{textAlign: 'center'}}>
+    <h1> "Your Health Our Priority" </h1>
+    <div className="heading"> RK HOSPITAL</div>
+    <Link to="/login" id="headinglink">Login</Link>
+    </div>
+    </>
+   
   )
 }
 
