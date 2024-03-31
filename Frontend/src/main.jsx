91641684988262ddfe9924fb4647a2217ebc0009
Kavita Sakariya <kavitasoni141103@gmail.com>
2024-03-31 22:13:11 +0530

    add routes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+// import App from './App.jsx'
 import './index.css'
+import HomePage from './Pages/HomePage.jsx'
+import LoginPage from './Pages/LoginPage.jsx';
+import RegisterPage from './Pages/RegisterPage.jsx';
+import Dashboard from './Pages/Dashboard.jsx';
+import PatientPage from './Pages/PatientPage.jsx';
+import DoctorPage from './Pages/DoctorPage.jsx';
 
+
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>,
+  },
+
+  {
+    path: "/login",
+    element: <LoginPage/>,
+  },
+
+  {
+    path: "/register",
+    element: <RegisterPage/>,
+  },
+
+  {
+    path:"/dashboard",
+    element: <Dashboard/>,
+  },
+
+  {
+    path:"/Patient",
+    element: <PatientPage/>,
+  },
+
+  {
+    path:"/doctor",
+    element: <DoctorPage/>,
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
