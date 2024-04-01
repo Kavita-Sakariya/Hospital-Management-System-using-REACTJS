f1a251517b1ef36c914a5f7eef7beb6cb36ff19e
Kavita Sakariya <kavitasoni141103@gmail.com>
2024-04-01 22:48:40 +0530

    Updated Navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,50 +1,59 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import * as Falcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
-import { Sidebar } from './Sidebar';
-import { IconContext } from 'react-icons';
-import './Navbar.css';
-
-
+import { Sidebar } from "./Sidebar";
+import { IconContext } from "react-icons";
+import "./Navbar.css";
 
 const Navbar = () => {
-    const [sidebar,setSidebar] = useState(true);
+  const [sidebar, setSidebar] = useState(true);
 
-    const showSidebar = () => setSidebar(!sidebar);
+  const showSidebar = () => setSidebar(!sidebar);
+ 
+
   return (
     <>
-    <IconContext.Provider value={{color:'black'}}>
-    <div className="navbarMain">
-    <div className="navbar">
-        <Link to="#" className='menu-bars'>
-        <Falcons.FaBars onClick={showSidebar}/>
-        </Link>
-    </div>
-        <nav className={sidebar ? 'nav-menu active': 'nav-menu'}>
-            <ul className='nav-menu-items' onClick={showSidebar}>
-                <li className='navbar-toggle'>
-                    <Link to="#" className='menu-bars'>
-                        <AiIcons.AiOutlineClose/>
+      <IconContext.Provider value={{ color: "black" }}>
+        <div className="navbarMain">
+          <div className="navbar">
+            <Link to="#" className="menu-bars">
+              <Falcons.FaBars onClick={showSidebar} />
+            </Link>
+          </div>
+          <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+            <ul className="nav-menu-items" onClick={showSidebar}>
+              <li className="navbar-toggle">
+                <Link to="#" className="menu-bars">
+                  <AiIcons.AiOutlineClose />
+                </Link>
+              </li>
+              {Sidebar.map((item, index) => {
+                return (
+                  <li key={index} className={item.cName}>
+                    <Link to={item.path}>
+                      {item.icon}
+                      <span>{item.title}</span>
                     </Link>
-                </li>
-                {Sidebar.map((item, index) => {
-              return (
-                <li key={index} className={item.cName}>
-                  <Link to={item.path}>
-                    {item.icon}
-                    <span>{item.title}</span>
-                  </Link>
-                </li>
-              );
-            })}
+                  </li>
+                );
+              })}
             </ul>
-        </nav>
-        <Link to="/" className='logout'>Log Out</Link>
-    </div>
-    </IconContext.Provider>
+            <div className="nav-dropdown">
+              <select className="dropdown-menu" defaultValue="Admin">
+                <option value="User">User</option>
+                <option value="Nurse">Nurse</option>
+                <option value="Admin">Admin</option>
+              </select>
+            </div>
+          </nav>
+          <Link to="/" className="logout">
+            Log Out
+          </Link>
+        </div>
+      </IconContext.Provider>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
